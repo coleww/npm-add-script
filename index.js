98fe9b0e6497c568a17e9d4115da366f690c0d72
Ahmed Shaaban <ahmedsh3ban1995@gmail.com>
2018-04-19 10:04:13 +0200

    Add root feature

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var jsonfile = require('jsonfile')
+var path = require('path');
 
-function ThatScriptsEntryAlreadyExistsThereInThePackageDotJsonMyFriendError (name) {
+function ThatScriptsEntryAlreadyExistsThereInThePackageDotJsonMyFriendError(name) {
   this.name = 'ThatScriptsEntryAlreadyExistsThereInThePackageDotJsonMyFriendError'
   this.message = 'My friend:\n  it seems as though the script entry you have specified,\n  "' + name + '"\n  is already present in the package.json file,\n  of your current working directory.\n  Please try again,\n  or amend this egregious error.\nThank You.'
 }
@@ -8,7 +9,7 @@ function ThatScriptsEntryAlreadyExistsThereInThePackageDotJsonMyFriendError (nam
 ThatScriptsEntryAlreadyExistsThereInThePackageDotJsonMyFriendError.prototype = new Error()
 ThatScriptsEntryAlreadyExistsThereInThePackageDotJsonMyFriendError.prototype.constructor = ThatScriptsEntryAlreadyExistsThereInThePackageDotJsonMyFriendError
 
-function YouDoNotAppearToBeInANodeProjectPerhapsUShouldRun_npm_init_error (name) {
+function YouDoNotAppearToBeInANodeProjectPerhapsUShouldRun_npm_init_error(name) {
   this.name = 'YouDoNotAppearToBeInANodeProjectPerhapsUShouldRun_npm_init_error'
   this.message = 'My friend:\n  it seems as though your current working directory\n  does not contain a package.json file,\n  and is therefore not a Node.js project.\n  Please run npm init,\n  in order to amend this tremendous violation.\nThank You.'
 }
@@ -18,13 +19,13 @@ YouDoNotAppearToBeInANodeProjectPerhapsUShouldRun_npm_init_error.prototype.const
 
 module.exports = function (script) {
   try {
-    var packaged = jsonfile.readFileSync('package.json')
+    var packaged = (script.root && jsonfile.readFileSync('../../package.json')) || jsonfile.readFileSync(path.join(__dirname,'../../package.json'))
     if (!packaged.scripts) packaged.scripts = {}
     if (!script.force && packaged.scripts[script.key]) {
       throw new ThatScriptsEntryAlreadyExistsThereInThePackageDotJsonMyFriendError(script.key)
     }
     packaged.scripts[script.key] = script.value
-    jsonfile.writeFileSync('package.json', packaged, {spaces: 2})
+    jsonfile.writeFileSync('package.json', packaged, { spaces: 2 })
   } catch (e) {
     if (e.message === 'ENOENT, no such file or directory \'package.json\'') {
       throw new YouDoNotAppearToBeInANodeProjectPerhapsUShouldRun_npm_init_error()
